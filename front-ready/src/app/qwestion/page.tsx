aa3d510651dc1d4705e4375ef19e1acb4b20bd24
elfototo <e89617881302@gmail.com>
2025-09-04 16:27:44 +0700

    structures page qwestion

--- front-ready/src/app/qwestion/page.tsx
+++ front-ready/src/app/qwestion/page.tsx
@@ -35,104 +35,106 @@ export default function Qwestions() {
         </Link>
       </div>
 
-      <div>
-        <h2 className="text-2xl">Список</h2>
-        <ul className="gap-6 list-disc list-inside">
+      <div className="flex gap-6">
+
+        <div className="w-[20%]">
+          <h2 className="text-2xl">Список</h2>
+          <ul className="list-disc list-inside">
+            {questions.map((item) => (
+              <li
+                key={item.title}
+                className="flex items-center cursor-pointer text-gray-400  hover:text-black py-2 border-t-1 solid border-gray-300"
+              >
+                <a href={`#${item.title}`} className="flex items-center gap-2">
+                  
+                  <h3>{item.title}</h3>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="w-[80%]">
           {questions.map((item) => (
-            <li
-              key={item.title}
-              className="flex gap-2 items-center cursor-pointer text-green-600 hover:underline"
-            >
-              <a href={`#${item.title}`} className="flex items-center gap-2">
-                <BsStarFill size={9} />
-                <h3>{item.title}</h3>
-              </a>
-            </li>
+            <div key={item.id} id={item.title} className="my-10">
+              <h2 className="font-bold text-2xl">{item.title}</h2>
+
+              {Array.isArray(item.content) &&
+                item.content.map((block, index) => {
+                  switch (block.type) {
+                    case "title":
+                      return (
+                        <h3 key={index} className="my-4 font-bold">
+                          {(block as { text: string }).text}
+                        </h3>
+                      );
+
+                    case "paragraph":
+                      return (
+                        <p key={index} className="my-4">
+                          {(block as { text: string }).text}
+                        </p>
+                      );
+
+                    case "list":
+                      if (!("items" in block) || !Array.isArray(block.items))
+                        return null;
+
+                      return (
+                        <ul
+                          key={index}
+                          className="my-4 ml-4 list-disc border px-8 py-4 rounded-xl"
+                        >
+                          {block.items.map((li, i) => (
+                            <li key={i}>{li}</li>
+                          ))}
+                        </ul>
+                      );
+
+                    case "links":
+                      if (!("items" in block) || !Array.isArray(block.items))
+                        return null;
+
+                      return (
+                        <div
+                          key={index}
+                          className="my-4 mx-4 grid-cols-1 text-green-600"
+                        >
+                          {block.items.map((link, i) => (
+                            <a
+                              key={i}
+                              href={link}
+                              target="_blank"
+                              rel="noopen noreferrer"
+                              className="flex gap-2 items-center hover:underline"
+                            >
+                              <FaLink size={10} /> {link}
+                            </a>
+                          ))}
+                        </div>
+                      );
+
+                    case "code":
+                      if (!("code" in block && typeof block.code === "string"))
+                        return null;
+                      return (
+                        <SyntaxHighlighter
+                          key={index}
+                          language="javascript"
+                          style={oneDark}
+                          showLineNumbers
+                          customStyle={{ borderRadius: "8px", padding: "12px" }}
+                        >
+                          {block.code}
+                        </SyntaxHighlighter>
+                      );
+
+                    default:
+                      return null;
+                  }
+                })}
+            </div>
           ))}
-        </ul>
-      </div>
-      <div>
-        {questions.map((item) => (
-          <div key={item.id} id={item.title} className="my-10">
-            <h2 className="font-bold text-2xl">{item.title}</h2>
-
-            {Array.isArray(item.content) &&
-              item.content.map((block, index) => {
-                switch (block.type) {
-                  case "title":
-                    return (
-                      <h3 key={index} className="my-4 font-bold">
-                        {(block as { text: string }).text}
-                      </h3>
-                    );
-
-                  case "paragraph":
-                    return (
-                      <p key={index} className="my-4">
-                        {(block as { text: string }).text}
-                      </p>
-                    );
-
-                  case "list":
-                    if (!("items" in block) || !Array.isArray(block.items))
-                      return null;
-
-                    return (
-                      <ul
-                        key={index}
-                        className="my-4 ml-4 list-disc border px-8 py-4 rounded-xl"
-                        
-                      >
-                        {block.items.map((li, i) => (
-                          <li key={i}>{li}</li>
-                        ))}
-                      </ul>
-                    );
-
-                  case "links":
-                    if (!("items" in block) || !Array.isArray(block.items))
-                      return null;
-
-                    return (
-                      <div
-                        key={index}
-                        className="my-4 mx-4 grid-cols-1 text-green-600"
-                      >
-                        {block.items.map((link, i) => (
-                          <a
-                            key={i}
-                            href={link}
-                            target="_blank"
-                            rel="noopen noreferrer"
-                            className="flex gap-2 items-center hover:underline"
-                          >
-                            <FaLink size={10} /> {link}
-                          </a>
-                        ))}
-                      </div>
-                    );
-
-                  case "code":
-                    if (!("code" in block && typeof block.code === "string"))
-                      return null;
-                    return (
-                      <SyntaxHighlighter
-                        key={index}
-                        language="javascript"
-                        style={oneDark}
-                        showLineNumbers
-                        customStyle={{ borderRadius: "8px", padding: "12px" }}
-                      >
-                        {block.code}
-                      </SyntaxHighlighter>
-                    );
-
-                  default:
-                    return null;
-                }
-              })}
-          </div>
-        ))}
+        </div>
       </div>
     </div>
   );
